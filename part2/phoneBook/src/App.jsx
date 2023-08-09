5abf16ba43ad01fcb448a28fff3312eebd46c9d6
luckypyreshep <koziatek91@gmail.com>
2023-08-08 21:51:55 -0400

    Finished most of stage 2, need to break out into components

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import DisplayPersons from "./components/DisplayPersons";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const updateSearch = (event) => {
+    const value = event.target.value;
+
+    console.log(value);
+
+    if (value !== "") {
+      const searchResults = persons.filter((person) =>
+        person.name.includes(value)
+      );
+      setSearchResults(searchResults);
+    } else {
+      setSearchResults(null);
+    }
+  };
+
+  const alreadyExists = (e) => {
+    e.preventDefault();
+    const found = persons.find((person) => newName === person.name);
+    if (found !== undefined) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPersons = [...persons];
+      newPersons.push({ name: newName, number: newNumber });
+      setPersons(newPersons);
+    }
+    setNewName("");
+    setNewNumber("");
+  };
 
   return (
-    <>
+    <div>
+      <h2>Phonebook</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        filter shown with <input onChange={(e) => updateSearch(e)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <form onSubmit={alreadyExists}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {searchResults !== null
+        ? searchResults.map((result) => (
+            <div key={result.name}>{result.name}</div>
+          ))
+        : ""}
+      <DisplayPersons persons={persons} />
+    </div>
+  );
+};
 
-export default App
+export default App;
