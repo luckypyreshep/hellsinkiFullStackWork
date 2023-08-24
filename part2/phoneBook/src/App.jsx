02e653d340f375e8ec7fcdf63416f91630fc2bc6
luckypyreshep <koziatek91@gmail.com>
2023-08-24 14:26:54 -0400

    deleted import

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./index.css";
 import DisplayPersons from "./components/DisplayPersons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
