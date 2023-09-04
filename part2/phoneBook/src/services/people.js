873bc7cb619fb697ed6a06b399338e4e2ba8c6c8
luckypyreshep <koziatek91@gmail.com>
2023-09-04 17:22:37 -0400

    changes people service to use a relative link

--- part2/phoneBook/src/services/people.js
+++ part2/phoneBook/src/services/people.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   return axios.get(baseUrl);
 };
 
 const addPerson = (newPerson) => {
+  console.log(newPerson);
   return axios.post(baseUrl, newPerson);
 };
 
